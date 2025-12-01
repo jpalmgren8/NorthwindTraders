471ff572d5b09d6b10a7fdc9309d7e6fb2ffddae
Julian Palmgren <julianpalmgren@gmail.com>
2025-12-01 15:10:44 -0500

    Second commit, working with sql database

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -6,22 +6,53 @@ public class App {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         Class.forName("com.mysql.cj.jdbc.Driver");
 
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind",
-                "root",
-                "yearup");
+        ResultSet resultSet = null;
+        PreparedStatement preparedStatement = null;
+        Connection connection = null;
+        try {
 
-        Statement statement = connection.createStatement();
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind",
+                    "root",
+                    "yearup");
 
-        String query = "SELECT CompanyName FROM Customers";
+            preparedStatement = connection.prepareStatement(
+                    "SELECT ProductID, ProductName, UnitPrice, UnitsInStock FROM products WHERE ProductName LIKE ?");
 
-        ResultSet results = statement.executeQuery(query);
+            preparedStatement.setString(1, "Sa%");
 
-        while (results.next()) {
-            String name = results.getString("CompanyName");
-            System.out.println(name);
+            resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                System.out.printf("%s",
+                        resultSet.getString(1));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (resultSet != null) {
+                try {
+                    resultSet.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (preparedStatement != null) {
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+
         }
-
-        connection.close();
-
     }
 }
