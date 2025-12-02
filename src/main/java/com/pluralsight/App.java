17c6d45ab22d0671fac3c0518561967aefa17d69
Julian Palmgren <julianpalmgren@gmail.com>
2025-12-02 11:14:39 -0500

    Commit #3: 12/2/25
    * Worked on:
    - Adding customer query to load info on customers
    - Implemented while loop, scanner, and switch for user selection
    - User can select products, customers, or exit the program

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -1,30 +1,67 @@
 package com.pluralsight;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        boolean menuSelect = true;
+
+        while (menuSelect) {
+
+            System.out.println("What do you want to do?");
+        System.out.println("1) Display all products");
+        System.out.println("2) Display all customers");
+        System.out.println("0) Exit");
+        System.out.println("Select an option: ");
+
+        int userInput = sc.nextInt();
+        sc.nextLine();
+
+
+        switch (userInput) {
+            case 1:
+                productQueries();
+                break;
+            case 2:
+                customerQueries();
+                break;
+            case 0:
+                menuSelect = false;
+                System.out.println("Goodbye!");
+                System.exit(0);
+            }
+        }
+    }
+
+    public static void customerQueries() {
 
         ResultSet resultSet = null;
         PreparedStatement preparedStatement = null;
         Connection connection = null;
+
+        String query = "SELECT * FROM Customers ORDER BY Country";
+
         try {
 
             connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind",
                     "root",
                     "yearup");
 
-            preparedStatement = connection.prepareStatement(
-                    "SELECT ProductID, ProductName, UnitPrice, UnitsInStock FROM products WHERE ProductName LIKE ?");
-
-            preparedStatement.setString(1, "Sa%");
+            preparedStatement = connection.prepareStatement(query);
 
             resultSet = preparedStatement.executeQuery();
 
             while (resultSet.next()) {
-                System.out.printf("%s",
-                        resultSet.getString(1));
+                System.out.println("Contact Name: " + resultSet.getString("ContactName"));
+                System.out.println("Company: " + resultSet.getString("CompanyName"));
+                System.out.println("City: " + resultSet.getString("City"));
+                System.out.println("Country: " + resultSet.getString("Country"));
+                System.out.println("Phone #: " + resultSet.getString("Phone"));
+                System.out.println("------------------");
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -52,7 +89,60 @@ public class App {
                     e.printStackTrace();
                 }
             }
+        }
+    }
 
+    public static void productQueries() {
+
+        ResultSet resultSet = null;
+        PreparedStatement preparedStatement = null;
+        Connection connection = null;
+
+        String query = "SELECT * FROM Products";
+
+        try {
+
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind",
+                    "root",
+                    "yearup");
+
+            preparedStatement = connection.prepareStatement(query);
+
+            resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                System.out.println("Product ID: " + resultSet.getInt("ProductID"));
+                System.out.println("Name: " + resultSet.getString("ProductName"));
+                System.out.println("Price: " + resultSet.getFloat("UnitPrice"));
+                System.out.println("Stock: " + resultSet.getInt("UnitsInStock"));
+                System.out.println("------------------");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (resultSet != null) {
+                try {
+                    resultSet.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (preparedStatement != null) {
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
